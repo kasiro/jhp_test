0c3904f89ee3918bedf062537cf066778a338379
kasiro <kon.konikov@mail.ru>
2021-10-06 19:44:08 +0700

    edit

--- ModulePattern.php
+++ ModulePattern.php
@@ -1,11 +1,41 @@
 <?php
 
 class JhpModule {
+	protected $settings = [];
+	protected $name;
 	public static $list = [];
-	public static $listArgs = [];
+
+	private function PrepareName(string $path){
+		return explode('.', basename($path))[0];
+	}
+
+	public function setName(string $name){
+		$newName = $this->PrepareName($name);
+		$this->name = $newName;
+	}
+
+	public function getName(){
+		return $this->name;
+	}
+
+	public function setSettings(array $settings){
+		$this->settings = $settings;
+	}
+
+	public function getSettings(){
+		return $this->settings;
+	}
+
+	// public function action($function){
+	// 	return $function();
+	// }
 
 	public static function addReg(string $regexp, string|callable $function){
-		static::$list[$regexp] = $function;
+		if (!array_key_exists($regexp, static::$list)){
+			static::$list[$regexp] = $function;
+		} else {
+			throw new Exception('regexp is aelredy in $list');
+		}
 	}
 
 	public function handle($code){
--- modules/js_else.php
+++ modules/js_else.php
@@ -1,6 +1,7 @@
 <?php
 
 $module = new JhpModule;
+$module->setName(__FILE__);
 $module->addreg('/(var|let) \{((?:(?(R).*|[^}]*+)|(?R))*)\} = (.*)/m', 'list($2) = $3');
 $module->addreg('/(var|let) \[((?:(?(R).*|[^]]*+)|(?R))*)\] = (.*)/m', 'list($2) = $3');
 return $module;
